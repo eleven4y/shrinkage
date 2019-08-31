ec1f0112dc73ea70e7e1f315014ed8ba2ce8cb60
Konstantin Petrov <kpetrov@spbfiller.com>
2019-09-01 01:34:11 +0300

    [bugfix] remove error params and throw it on 'disconnected' handler of database

--- server/source/db/handlers/components/disconnected.js
+++ server/source/db/handlers/components/disconnected.js
@@ -1,10 +1,8 @@
 /**
- * @throws
  * @param {string} mark mark of instance
  * @param {string} connectionString string of connected database location
  * @return {Function}
  */
-export default (mark, connectionString) => (error) => {
-  console.error(`${mark} disconnected on ${connectionString}`, error);
-  throw error;
+export default (mark, connectionString) => () => {
+  console.log(`${mark} disconnected on ${connectionString}`);
 };
