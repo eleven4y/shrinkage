81c053b593af380bbdaa50f52d3fd73bee4909eb
Konstantin Petrov <kpetrov@spbfiller.com>
2019-09-01 01:10:46 +0300

    [feature] add hooks for user model
    add hooks on model 'save', which hash user password
    add method for model, whick compare plain text and user password

--- server/source/models/user.js
+++ server/source/models/user.js
@@ -1,5 +1,7 @@
 import mongoose, { Schema } from 'mongoose';
 
+import { hash, compare } from '../middlewares/bcrypt';
+
 export const MODEL_NAME = 'user';
 export const COLLECTION_NAME = 'users';
 
@@ -19,5 +21,18 @@ const UserSchema = new Schema({
     required: true,
   }
 }, { timestamps: true });
+
+UserSchema.pre('save', function(next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  this.password = hash(this.password);
+  return next();
+});
+
+UserSchema.methods.comparePassword = function(text) {
+  return compare(text, this.password);
+}
 
 export default mongoose.model(MODEL_NAME, UserSchema, COLLECTION_NAME);
