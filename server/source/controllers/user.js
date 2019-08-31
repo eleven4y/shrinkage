d4f3f9b82fc15ede56891642486a683733251a49
Konstantin Petrov <kpetrov@spbfiller.com>
2019-09-01 01:19:39 +0300

    [feauter] modify user controller, add actions
    add update user by id action
    add delete user by id action

--- server/source/controllers/user.js
+++ server/source/controllers/user.js
@@ -1,7 +1,7 @@
 import UserModel from '../models/user';
 
 /**
- * Create new user with given params on request body
+ * Create new user with given params
  * @param {Object} req global http request object
  * @param {Object} req.body object with request body parameters
  * @param {string} req.body.login user login
@@ -26,7 +26,7 @@ export async function createUser(req, res, next) {
 }
 
 /**
- * Get user parameters by unique identificator on request query
+ * Get user parameters by unique identificator
  * @param {Object} req global http request object
  * @param {Object} req.query object with request query parameters
  * @param {string} req.query.id unique identificator of user
@@ -40,6 +40,59 @@ export async function getUserById(req, res, next) {
 
   try {
     user = await UserModel.findOne({ _id: id });
+  } catch (error) {
+    next(error);
+  }
+
+  return res
+    .status(200)
+    .json(user);
+}
+
+/**
+ * Find user by unique identificator and update his parametes
+ * @param {Object} req global http request object
+ * @param {Object} req.body object with request body parameters
+ * @param {string} req.body.login user login
+ * @param {Object} req.query object with request query parameters
+ * @param {string} req.query.id unique identificator of user
+ * @param {Object} res global http response object
+ * @param {Function} next express next function
+ * @return {Object}
+ */
+export async function updateUserById(req, res, next) {
+  const { id } = req.query;
+  const { login } = req.body;
+  let user;
+
+  try {
+    user = await UserModel.findOneAndUpdate({ _id: id }, { login });
+  } catch (error) {
+    next(error);
+  }
+
+  return res
+    .status(200)
+    .json(user);
+}
+
+/**
+ * Find user by unique identificator and delete
+ * @param {Object} req global http request object
+ * @param {Object} req.body object with request body parameters
+ * @param {string} req.body.login user login
+ * @param {Object} req.query object with request query parameters
+ * @param {string} req.query.id unique identificator of user
+ * @param {Object} res global http response object
+ * @param {Function} next express next function
+ * @return {Object}
+ */
+export async function deleteUserById(req, res, next) {
+  const { id } = req.query;
+  let user;
+
+  try {
+    user = await UserModel.findOneAndDelete({ _id: id });
   } catch (error) {
     next(error);
   }
