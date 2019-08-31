a63bd6f7bd05da445e3048a2720c2cb5dc843e9a
Konstantin Petrov <kpetrov@spbfiller.com>
2019-09-01 00:38:36 +0300

    [feature] add user controller
    add create user action
    add get user by id action

---
+++ server/source/controllers/user.js
@@ -0,0 +1,50 @@
+import UserModel from '../models/user';
+
+/**
+ * Create new user with given params on request body
+ * @param {Object} req global http request object
+ * @param {Object} req.body object with request body parameters
+ * @param {string} req.body.login user login
+ * @param {string} req.body.password user password
+ * @param {Object} res global http response object
+ * @param {Function} next express next function
+ * @return {Object}
+ */
+export async function createUser(req, res, next) {
+  const { login , password } = req.body;
+  let user;
+
+  try {
+    user = await UserModel.create({ login, password });
+  } catch (error) {
+    next(error);
+  }
+
+  return res
+    .status(200)
+    .json(user);
+}
+
+/**
+ * Get user parameters by unique identificator on request query
+ * @param {Object} req global http request object
+ * @param {Object} req.query object with request query parameters
+ * @param {string} req.query.id unique identificator of user
+ * @param {Object} res global http response object
+ * @param {Function} next express next function
+ * @return {Object}
+ */
+export async function getUserById(req, res, next) {
+  const { id } = req.query;
+  let user;
+
+  try {
+    user = await UserModel.findOne({ id });
+  } catch (error) {
+    next(error);
+  }
+
+  return res
+    .status(200)
+    .json(user);
+}
