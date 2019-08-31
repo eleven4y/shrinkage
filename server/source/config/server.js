c8181077b3bfe7404817fcb942fcaf1441c69f45
Konstantin Petrov <kpetrov@spbfiller.com>
2019-08-31 15:39:34 +0300

    [feature] add server config

---
+++ server/source/config/server.js
@@ -0,0 +1,13 @@
+/**
+ * Configuration of used server
+ * @property {string} name
+ * @property {string} host
+ * @property {number} port
+ */
+const config = {
+  name: 'express',
+  host: 'localhost',
+  port: 3001,
+};
+
+export default config;
