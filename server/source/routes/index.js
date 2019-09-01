78f314e17d3ebfbc33156558f2564d421496942c
Konstantin Petrov <kpetrov@spbfiller.com>
2019-09-02 00:45:38 +0300

    [feature] rename user router and controller, add get user action
    rename user router to users
    rename user controller to users
    add action for get list of users

--- server/source/routes/index.js
+++ server/source/routes/index.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 
-import userRouter from './user';
+import usersRouter from './users';
 
 const router = Router();
 
-router.use('/user', userRouter);
+router.use('/users', usersRouter);
 
 export default router;
