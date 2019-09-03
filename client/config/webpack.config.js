0997e846b794f4e9d90d40d21c37dd950565ba5a
Konstantin Petrov <kpetrov@spbfiller.com>
2019-09-03 20:53:30 +0300

    [config] add webpack alias for global styles

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -292,6 +292,8 @@ module.exports = function(webpackEnv) {
         containers: path.resolve(__dirname, '../src/containers'),
         locales: path.resolve(__dirname, '../src/locales'),
         services: path.resolve(__dirname, '../src/services'),
+
+        styles: path.resolve(__dirname, './styles'),
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
